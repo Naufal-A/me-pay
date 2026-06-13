0773a3939c1a3920507d661d46a5174cd57157fb
Naufal Aulia <muhammadnaufal.a333@gmail.com>
2026-06-13 15:01:06 +0700

    feat: change middleware  to proxy for role-based access control and URL redirection

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   // 1. Ambil tiket (cookie) yang tadi kita buat pas login
   const roleCookie = request.cookies.get("userRole");
   const userRole = roleCookie?.value;
